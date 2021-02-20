2c3864c93f63578a68e189ab87108cd67483f569
Joe Lott <rjplott@gmail.com>
2021-02-20 18:20:48 -0500

    complete exercise 2.1

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -11,18 +11,9 @@ const Part = (props) => (
 
 const Content = (props) => (
   <div>
-    <Part
-      part={props.course.parts[0].name}
-      exercise={props.course.parts[0].exercises}
-    />
-    <Part
-      part={props.course.parts[1].name}
-      exercise={props.course.parts[1].exercises}
-    />
-    <Part
-      part={props.course.parts[2].name}
-      exercise={props.course.parts[2].exercises}
-    />
+    {props.course.parts.map((part) => (
+      <Part part={part.name} exercise={part.exercises} />
+    ))}
   </div>
 );
 
@@ -35,32 +26,43 @@ const Total = (props) => (
   </p>
 );
 
+const Course = ({ course }) => (
+  <div>
+    <Header course={course} />
+    <Content course={course} />
+    <Total course={course} />
+  </div>
+);
+
 const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
+      },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
       },
     ],
   };
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  );
+  return <Course course={course} />;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
